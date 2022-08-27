bcddf735e04376a0eda2a01262d95e32dfa82aa4
Big bank <6310110234@psu.ac.th>
2022-08-27 18:50:12 +0700

    Part4

--- components/Weather.js
+++ components/Weather.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
-import { ImageBackground, Text, StyleSheet, View} from 'react-native';
+import { ImageBackground, Text, StyleSheet, View } from 'react-native';
 import Forecast from './Forecast';
 import Constants from 'expo-constants';
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=f64db5994141cbe3ca115dd0799fa05f`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
         description: 'description',
         temp: 0
     });
     return (
-            <ImageBackground source={require("../bg.png")} style = {styles.backdrop}>
-                <View style = {styles.highlight}>
-                    <Text style = {styles.fontsize}>Zip Code is {props.zipCode}</Text> 
-                    <Forecast {...forecastInfo} />
-                </View>
-                
-            </ImageBackground>
+        <ImageBackground source={require("../bg.png")} style={styles.backdrop}>
+            <View style={styles.highlight}>
+                <Text style={styles.fontsize}>Zip Code is {props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </View>
+
+        </ImageBackground>
     );
 }
 const styles = StyleSheet.create({
@@ -45,4 +63,3 @@ const styles = StyleSheet.create({
 
 
 });
-   
